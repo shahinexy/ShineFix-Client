b8a77ff516b0717ca4669dfd5927ca2401f455f1
shahinexy <shahinexy@gmail.com>
2024-05-10 18:23:49 +0600

    404 page created

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,15 +1,22 @@
-import { Button } from '@nextui-org/react';
-import errorLogo from '../../assets/images/oops-404-error-with-a-broken-robot-animation-flash-message-color-failed-loading-animation-for-broken-link-web-design-animation-ultra-hd-4k-fre.jpg'
-import { Link } from 'react-router-dom';
+import { Button } from "@nextui-org/react";
+import errorLogo from "../../assets/images/404-landing-page-free-vector.jpg";
+import { Link } from "react-router-dom";
 const ErrorPage = () => {
-    return (
-        <div className='flex h-screen w-full items-center justify-center'>
-            <div className='text-center'>
-                <img src={errorLogo} alt="" />
-                <Link to={'/'}><Button className='text-xl rounded-none bg-primary dark:bg-secondary text-white font-medium px-10 py-7 mt-8'>Back To Home</Button></Link>
-            </div>            
-        </div>
-    );
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="text-center space-y-5">
+        <img src={errorLogo} alt="" />
+        <h2 className="text-primary text-4xl font-bold">
+          Oops!! Page Not Found
+        </h2>
+        <Link to={"/"}>
+          <Button className="text-xl rounded-none bg-primary dark:bg-secondary text-white font-medium px-10 py-7 mt-7">
+            Back To Home
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
