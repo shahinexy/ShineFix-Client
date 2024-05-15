725b5825e279fdb62ca1c1a49398d915cf534c5c
shahinexy <shahinexy@gmail.com>
2024-05-15 16:42:23 +0600

    serach function issue fixed

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -7,15 +7,21 @@ import { FaAngleDoubleRight, FaAngleDown, FaSearch } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { GiTireIronCross } from "react-icons/gi";
-import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react";
 
 const Services = () => {
   const [isVisible, setIsVisisble] = useState(false);
   const [search, setSearch] = useState([]);
   const [searchFilter, setSearchFilter] = useState("");
-  const [asc, setAsc] = useState('non')
+  const [asc, setAsc] = useState("non");
 
-  const { data, isPending, isError, error, refetch } = useQuery({
+  const { data, isPending, isError, error } = useQuery({
     queryKey: ["serviceSection", asc],
     queryFn: async () => {
       const res = await axios.get(
@@ -31,6 +37,7 @@ const Services = () => {
   }, [data]);
 
   console.log(asc);
+  console.log(searchFilter.toLowerCase());
 
   if (isPending) return <Loader></Loader>;
   if (isError) console.log(error.message);
@@ -42,34 +49,35 @@ const Services = () => {
       </Helmet>
       <div className="md:w-4/6 my-10">
         <h1 className="md:text-4xl text-3xl font-bold text-primary dark:text-secondary uppercase mb-4">
-        Your One-Stop Solution for Home Excellence
+          Your One-Stop Solution for Home Excellence
         </h1>
         <p>
-        Discover a comprehensive range of expert services tailored to elevate your home. From renovations to repairs, plumbing to painting, trust our skilled team to deliver exceptional results with professionalism and care. Experience the convenience of finding all your home improvement needs under one roof with ShineFix.
+          Discover a comprehensive range of expert services tailored to elevate
+          your home. From renovations to repairs, plumbing to painting, trust
+          our skilled team to deliver exceptional results with professionalism
+          and care. Experience the convenience of finding all your home
+          improvement needs under one roof with ShineFix.
         </p>
       </div>
 
-      <div className="relative max-w-5xl mx-auto flex justify-between gap-3 md:my-10 my-5">
+      <div className="relative max-w-5xl mx-auto flex justify-between flex-wrap-reverse gap-3 md:my-10 my-5">
         <div>
-          
-        <Dropdown className="bg-white dark:bg-[#31363F] shadow-lg shadow-secondary rounded-none">
+          <Dropdown className="bg-white dark:bg-[#31363F] shadow-lg shadow-secondary rounded-none">
             <DropdownTrigger>
               <Button
                 className={`bg-primary dark:bg-secondary border border-primary dark:border-secondary rounded-none text-lg text-white`}
               >
-                Sort by price <FaAngleDown/>
+                Sort by price <FaAngleDown />
               </Button>
             </DropdownTrigger>
             <DropdownMenu aria-label="Static Actions">
-              <DropdownItem onClick={() => setAsc('asc')}>
+              <DropdownItem onClick={() => setAsc("asc")}>
                 Low to high
               </DropdownItem>
-              <DropdownItem onClick={() => setAsc('dsc') }>
+              <DropdownItem onClick={() => setAsc("dsc")}>
                 High to low
               </DropdownItem>
-              <DropdownItem onClick={() => setAsc('non')}>
-                Default
-              </DropdownItem>
+              <DropdownItem onClick={() => setAsc("non")}>Default</DropdownItem>
             </DropdownMenu>
           </Dropdown>
         </div>
@@ -78,10 +86,9 @@ const Services = () => {
           <input
             onChange={(e) => setSearchFilter(e.target.value)}
             onFocus={() => setIsVisisble(true)}
-            // onBlur={() => setIsVisisble(false)}
             className="py-2 focus:outline-none bg-white dark:bg-white/0"
             type="text"
-            placeholder="search now"
+            placeholder="write in lower care"
           />
           <FaSearch className="text-xl text-primary dark:text-secondary" />
         </div>
@@ -89,16 +96,15 @@ const Services = () => {
         {isVisible && (
           <div className="absolute top-14 right-0 z-20 sm:w-[500px] w-full bg-[#eef7ff] dark:bg-primary shadow-lg shadow-secondary px-5 py-2 border border-primary dark:border-secondary">
             <div className="flex justify-end">
-              <button onClick={() =>setIsVisisble(false)}>
-              <GiTireIronCross className="text-primary dark:text-white text-4xl font-bold border-2 border-primary dark:border-white rounded-full p-2" />
+              <button onClick={() => setIsVisisble(false)}>
+                <GiTireIronCross className="text-primary dark:text-white text-4xl font-bold border-2 border-primary dark:border-white rounded-full p-2" />
               </button>
-
             </div>
             {search
               .filter((item) => {
                 return searchFilter.toLowerCase() === ""
                   ? item
-                  : item.serviceName.toLowerCase().includes(searchFilter);
+                  : item.serviceName.toLowerCase().includes(searchFilter.toLowerCase());
               })
               .slice(0, 3)
               .map((data) => (
@@ -108,7 +114,10 @@ const Services = () => {
                   <div className="flex justify-between flex-wrap">
                     <p>{data.providerName}</p>
                     <p>price: {data.servicePrice}$</p>
-                    <Link to={`/serviceDetails/${data._id}`} className="flex items-center justify-center duration-500 hover:scale-110 hover:-rotate-2">
+                    <Link
+                      to={`/serviceDetails/${data._id}`}
+                      className="flex items-center justify-center duration-500 hover:scale-110 hover:-rotate-2"
+                    >
                       Details &nbsp; ----
                       <FaAngleDoubleRight className="text-2xl mt-[2px]"></FaAngleDoubleRight>
                     </Link>
